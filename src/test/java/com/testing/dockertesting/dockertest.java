54b1f1f639fe19858fce8c9258bf62820f8ee58b
gayathrikk <gayathri@htic.iitm.ac.in>
2023-08-08 13:03:27 +0530

    first commit

--- src/test/java/com/testing/dockertesting/dockertest.java
+++ src/test/java/com/testing/dockertesting/dockertest.java
@@ -3,24 +3,36 @@ package com.testing.dockertesting;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.By;
-
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class dockertest {
 
-	public static void main(String[] args) throws MalformedURLException, InterruptedException
-	{
-		
-		DesiredCapabilities dc= DesiredCapabilities.chrome();
-		URL url=new URL("http://localhost:4444/wd/hub");
-		RemoteWebDriver driver = new RemoteWebDriver(url,dc);
-		driver.get("http://public.humanbrain.in");
-		System.out.println("title"+driver.getTitle());
-		driver.quit();
-		
+    private RemoteWebDriver driver;
 
-	}
+    @BeforeTest
+    public void setup() throws MalformedURLException {
+        DesiredCapabilities dc = DesiredCapabilities.chrome();
+        URL url = new URL("http://localhost:4444/wd/hub");
+        driver = new RemoteWebDriver(url, dc);
+    }
 
-}
+    @Test
+    public void testWebsiteTitle() {
+        driver.get("http://public.humanbrain.in");
+        String title = driver.getTitle();
+        System.out.println("Title of the page=" +title);
+    }
+
+    @AfterTest
+    public void close() {
+       
+            driver.quit();
+        }
+    }
+
